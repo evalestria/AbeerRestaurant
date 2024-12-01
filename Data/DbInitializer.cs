ecb8f63106c63fca3b80016ed099a7e0cda09a95
evalestria <evalestria@gmail.com>
2024-12-01 13:31:15 +0000

    Added Database and Database Initializer

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using AbeerRestaurant.Models;
+using System.Linq;
+
+namespace AbeerRestaurant.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AbeerRestaurantContext context)
+        {
+            if (context.FoodItem.Any())
+            {
+                return;
+            }
+
+            var fooditems = new FoodItem[]
+            {
+                new FoodItem{Item_name="Apple Pie", Item_desc="Our tasty Apple Pie packed with cream.", Available=true, Vegetarian=true, Price=9}
+            };
+
+            context.FoodItem.AddRange(fooditems);
+            context.SaveChanges();
+        }
+    }
+}
