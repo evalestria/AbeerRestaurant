78018f83f4f260583019127c914ebc9335b5bd0b
Abeer <evalestria@gmail.com>
2025-03-02 10:14:00 +0000

    Menu Search Feature

--- Pages/Menu/View.cshtml.cs
+++ Pages/Menu/View.cshtml.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using AbeerRestaurant.Data;
 using AbeerRestaurant.Models;
 
@@ -18,12 +19,19 @@ namespace AbeerRestaurant.Pages.Menu
         }
 
         public IList<FoodItem> FoodItem { get; set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string SearchQuery { get; set; } = string.Empty;
 
         public async Task OnGetAsync()
         {
-            FoodItem = await _context.FoodItem
-                                     .Where(f => f.Available) // Only show available items
-                                     .ToListAsync();
+            IQueryable<FoodItem> query = _context.FoodItem.Where(f => f.Available);
+
+            if (!string.IsNullOrEmpty(SearchQuery))
+            {
+                query = query.Where(f => f.Item_name.Contains(SearchQuery) || f.Item_desc.Contains(SearchQuery));
+            }
+
+            FoodItem = await query.ToListAsync();
         }
     }
 }
