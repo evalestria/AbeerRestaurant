7428304b9c94dfc36767ea14157a6af8d649d588
Abeer <evalestria@gmail.com>
2025-03-02 09:46:08 +0000

    Implemented ContactUs with email messages and google map view

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+public class ContactModel : PageModel
+{
+    [BindProperty]
+    public ContactFormModel ContactForm { get; set; } = new ContactFormModel();
+
+    public void OnGet()
+    {
+    }
+
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
